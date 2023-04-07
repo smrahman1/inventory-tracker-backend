c09ac68899551c324bfc9e3a832dab6419ec0da9
Sheikh Mahfuzur Rahman <mahfuzur@Mahfuzurs-Air.cgocable.net>
2023-04-06 23:22:42 -0400

    path testing

--- src/services/inventoryService.js
+++ src/services/inventoryService.js
@@ -1,4 +1,5 @@
 const Inventory = require('../db/models/inventory');
+const fs = require('fs');
 const { GoogleAuth } = require('google-auth-library');
 const { CLOUD_FUNCTION_URL, CREDENTIALS_PATH } = require('../utils/constants');
 
@@ -20,6 +21,9 @@ class InventoryService {
   }
 
   static async fetchInventoryItem() {
+    fs.readdirSync('/opt/render/project/src').forEach((file) => {
+      console.log(file);
+    });
     const auth = new GoogleAuth({ keyFilename: CREDENTIALS_PATH });
     const client = await auth.getIdTokenClient(CLOUD_FUNCTION_URL);
     const result = await client.request({ url: CLOUD_FUNCTION_URL });
