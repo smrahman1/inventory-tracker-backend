21cf76bfc1e5dde64ef299ab4adb9f212e9212ed
Sheikh Mahfuzur Rahman <mahfuzur@Mahfuzurs-Air.cgocable.net>
2023-04-06 23:36:11 -0400

    removed file check

--- src/services/inventoryService.js
+++ src/services/inventoryService.js
@@ -1,5 +1,4 @@
 const Inventory = require('../db/models/inventory');
-const fs = require('fs');
 const { GoogleAuth } = require('google-auth-library');
 const { CLOUD_FUNCTION_URL, CREDENTIALS_PATH } = require('../utils/constants');
 
@@ -21,9 +20,6 @@ class InventoryService {
   }
 
   static async fetchInventoryItem() {
-    fs.readdirSync('/opt/render/project/src').forEach((file) => {
-      console.log(file);
-    });
     const auth = new GoogleAuth({ keyFilename: CREDENTIALS_PATH });
     const client = await auth.getIdTokenClient(CLOUD_FUNCTION_URL);
     const result = await client.request({ url: CLOUD_FUNCTION_URL });
